749fcffb67b79ced12fd075d137619a730ecf547
pedro <pedbcrespo@gmail.com>
2023-10-18 18:46:17 -0300

    atualizando

--- funcoes.py
+++ funcoes.py
@@ -24,7 +24,7 @@ def le_arquivo(nome_arquivo):
         linhas = [linha.strip() for linha in arquivo]
         for i in range(2):
             dicionario_arquivo.update(tratamento_funcao[f"{i}"](linhas[i]))
-            
+
         num_restricao = int(dicionario_arquivo['meta']['num_restricao'])
         dicionario_arquivo['sa'] = tratamento_funcao['sa'](linhas[2:2+num_restricao])
         eh_solucao = False
@@ -35,7 +35,6 @@ def le_arquivo(nome_arquivo):
             if '//' in linha:
                 continue
         return dicionario_arquivo
-
 
 # FUNCOES PARA REALIZAR CALCULOS DE MATRIZES
 def adiciona_valor_A(A, exp, valor):
@@ -49,7 +48,7 @@ def adiciona_valor_A(A, exp, valor):
 def forma_canonica(A, sinal):
     novo_A = A
     for i in range(len(A)):
-        if sinal[i] == '<=': 
+        if sinal[i] == '<=':
             novo_A = adiciona_valor_A(novo_A, novo_A[i], 1.0)
         elif sinal[i] == '>=':
             novo_A = adiciona_valor_A(novo_A, novo_A[i], -1.0)
@@ -64,46 +63,55 @@ def tratamento_sa(dados_arquivo, sinal_index):
     c = dados_arquivo['coeficiente']
     sinais = []
     pares_sinais_inversos = {'>=':'<=','<=':'>=','==':'=='}
-    for exp in sa:       
+    for exp in sa:
         b.append(exp[-1] if exp[-1] >= 0 else (exp[-1]*-1))
-        A.append(exp[:sinal_index] if exp[-1] >= 0 else list(map(lambda x: x*(-1), exp[:sinal_index])))
+        A.append(exp[:sinal_index+1] if exp[-1] >= 0 else list(map(lambda x: x*(-1), exp[:sinal_index])))
         sinais.append(exp[sinal_index+1] if exp[-1] >= 0 else pares_sinais_inversos[exp[sinal_index+1]])
     A = forma_canonica(A, sinais)
-    # while len(c) < len(A[0]):
-    #     c.append(0.0)
     return np.array(A), np.array(b), np.array(c)
 
 def gera_combinacoes(qtd_linhas, qtd_colunas):
     valores = [i for i in range(qtd_linhas)]
     combinacoes = permutations(valores)
-    return combinacoes
-    
-def gera_matriz_B(A, combinacao_colunas):
-    A_transposta = A.T
-    matriz_b = np.array([A_transposta[i] for i in combinacao_colunas])
-    return matriz_b.T
+    lista_combinacoes = []
+    for combinacao in combinacoes:
+      lista_combinacoes.append(list(combinacao))
+    return lista_combinacoes
 
-def gera_solucao(B, b):
+def gera_matriz_B_R(A, combinacao_colunas):
+    A_transposta = A.T
+    num_linhas, num_colunas = A.shape
+    combinacao_R = list(filter(lambda x: x not in combinacao_colunas, [i for i in range(num_colunas)]))
+    matriz_b = np.array([A_transposta[i] for i in combinacao_colunas])
+    matriz_r = np.array([A_transposta[i] for i in combinacao_R])
+    return np.array(matriz_b.T)
+
+def gera_solucao(B,b):
     B_inversa = np.linalg.inv(np.array(B))
     solucao = np.dot(B_inversa, b)
     return solucao
 
 def buscar_melhor_solucao(solucoes, c):
-    resultados = []
-    for solucao in solucoes:
-        linha_resultado = np.dot(c, solucao)
-        resultados.append(ft.reduce(lambda a, b: a+b,linha_resultado))
-    # return max(resultados)
-    return None
+    resultados = [np.dot(c, solucao) for solucao in solucoes]
+    return max(resultados)
+
+def junta(matriz_solucao, num_variaveis, combinacao):
+  solucao = []
+  for i in range(int(num_variaveis)):
+      valor = 0.0
+      if i in combinacao:
+        index = combinacao.index(i)
+        valor = matriz_solucao[index]
+      solucao.append(valor)
+  return np.array(solucao)
 
 def simplex(dados_arquivo):
     A, b, c = tratamento_sa(dados_arquivo, int(dados_arquivo['meta']['num_var_original']-1))
-    qtd_linha_A = len(A)
-    qtd_coluna_A = len(A[0])
+    qtd_linha_A, qtd_coluna_A = A.shape
     combinacoes = gera_combinacoes(qtd_linha_A, qtd_coluna_A)
     solucoes = []
     for combinacao in combinacoes:
-        B = gera_matriz_B(A, combinacao)
+        B, R = gera_matriz_B_R(A, combinacao, qtd_coluna_A)
         print("MATRIZ B")
         print(B)
         print("==============================")
