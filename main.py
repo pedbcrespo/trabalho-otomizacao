2750c504a87d7e4a9ded746adda10b42b461d97f
pedro <pedbcrespo@gmail.com>
2023-10-27 17:44:43 -0300

    implementando menu

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
 import funcoes as fc
+import os
 
-if __name__ == "__main__":
-    fc.main()
+def clear():
+    sistema = os.name
+    if sistema == "posix":
+        os.system("clear")
+    elif sistema == "nt":
+        os.system("cls")
+
+def opcao1():
+    arquivo = input('nome do arquivo: ')
+    return fc.executa(arquivo)
+
+def opcao2():
+    arquivo = input('nome do arquivo: ')
+    return fc.executa(arquivo)
+
+def opcao3():
+    print('Saindo do programa.')
+    return None
+
+
+array_funcao = [opcao1,opcao2, opcao3]
+while True:
+    print("Menu:")
+    print("1) Executar o Simplex, mostrando resultados intermediários")
+    print("2) Executar o Simplex, mostrando apenas o resultado final")
+    print("3) Sair do programa")
+
+    opcao = int(input("Escolha uma opção: ")) -1
+    if opcao > len(array_funcao):
+        clear()
+        continue
+    elif opcao == 2:
+        array_funcao[opcao]()
+        break
+    array_funcao[opcao]()
